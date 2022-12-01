d4bea3eae0f03228c59cc9579d42b4ddc2d2af2c
Vasiltsova Natalia <vasiltsova.natalia0103@gmail.com>
2022-12-02 00:03:53 +0300

    thanks for the evening, my favorite base

---
+++ src/main/java/com/example/database/EditTableDeliveryController.java
@@ -0,0 +1,45 @@
+package com.example.database;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class EditTableDeliveryController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField id_amount;
+
+    @FXML
+    private Button id_buttonEdit;
+
+    @FXML
+    private ComboBox<?> id_comboBoxNomenclature;
+
+    @FXML
+    private TextField id_price;
+
+    @FXML
+    private TextField id_sum;
+
+    @FXML
+    void ButtonEdit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
--- src/main/java/com/example/database/RegistrationController.java
+++ src/main/java/com/example/database/RegistrationController.java
@@ -1,10 +1,15 @@
 package com.example.database;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class RegistrationController {
 
@@ -33,10 +38,32 @@ public class RegistrationController {
     private TextField id_repeatPassword;
 
     @FXML
+    private Button id_avtoriz;
+
+    @FXML
     private Button id_sendCode;
 
     @FXML
     void initialize() {
+        id_avtoriz.setOnAction(event -> {
+            id_avtoriz.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("authorization.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+        });
+
 
 
     }
