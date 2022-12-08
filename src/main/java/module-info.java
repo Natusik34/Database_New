acbfe9bb1cabfe6e2291682c71432ee496bda795
Vasiltsova Natalia <vasiltsova.natalia0103@gmail.com>
2022-12-08 12:25:05 +0300

    сделала отправку кода на почту по видео

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.database {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires activation;
+    requires java.mail;
 
 
     opens com.example.database to javafx.fxml;
